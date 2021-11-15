f1baf886864ed4ea5fa1f9e2fde77bc27c7edcc1
Steve Beck <sbeck@inspiredtesting.com>
2021-11-15 08:47:09 +0200

    Added api challenge

---
+++ src/test/java/ApiChallenge.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+public class ApiChallenge {
+
+    /**
+     * Using OkHttp and the following Reset Server https://jsonplaceholder.typicode.com
+     * You are expected to deserialize the response data
+     */
+
+
+    /**
+     * Using the /users/n endpoint get the 5th user from typicode
+     */
+    @Test
+    public void Get5thUser()
+    {
+
+    }
+
+    /**
+     * Retrieve all the users from the /users endpoint and store them in a list
+     */
+    @Test
+    public void GetAllUsers()
+    {
+
+    }
+
+    /**
+     * Retrieve all the users from the /users endpoint, then find a user with the following street address
+     * Dayna Park
+     */
+    @Test
+    public void GetAllUsersAndRefine()
+    {
+
+    }
+}
